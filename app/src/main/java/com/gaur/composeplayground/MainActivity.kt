f7af071c04c53c2f468d41a20ba7c196d73b0b36
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2021-09-14 23:58:06 +0530

    Column in compose

--- app/src/main/java/com/gaur/composeplayground/MainActivity.kt
+++ app/src/main/java/com/gaur/composeplayground/MainActivity.kt
@@ -3,7 +3,10 @@ package com.gaur.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,7 +14,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gaur.composeplayground.ui.theme.ComposePlaygroundTheme
 
@@ -21,77 +23,50 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposePlaygroundTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    RowExample()
+                    ColumnExample()
                 }
             }
         }
     }
 }
 
-@Preview
-@Composable
-fun RowExample() {
 
-    Row(
+@Composable
+fun ColumnExample() {
+
+    Column(
         modifier = Modifier
-            .padding(12.dp)
-            .fillMaxWidth()
-            .wrapContentHeight()
+            .padding(8.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Card(
-            modifier = Modifier
-                .padding(8.dp)
-                .width(100.dp)
-                .height(100.dp)
-        ) {
 
-
-            Box(modifier = Modifier.fillMaxSize()) {
-
-                Text(
-                    text = "1", modifier = Modifier
-                        .padding(8.dp)
-                        .align(Alignment.Center)
-                )
-            }
-
-
+        Card(modifier = Modifier.padding(8.dp)) {
+            Text(text = "first card", modifier = Modifier.padding(8.dp))
         }
 
-
-        Card(
-            modifier = Modifier
-                .padding(8.dp)
-                .width(100.dp)
-                .height(100.dp)
-        ) {
-
-
-            Box(modifier = Modifier.fillMaxSize()) {
-
-                Text(
-                    text = "2", modifier = Modifier
-                        .padding(8.dp)
-                        .align(Alignment.Center)
-                )
-            }
-
-
+        Card(modifier = Modifier.padding(8.dp)) {
+            Text(text = "sec card", modifier = Modifier.padding(8.dp))
         }
 
+        Card(modifier = Modifier.padding(8.dp)) {
+            Text(text = "third card", modifier = Modifier.padding(8.dp))
+        }
+
+        Card(modifier = Modifier.padding(8.dp)) {
+            Text(text = "fourth card", modifier = Modifier.padding(8.dp))
+        }
+
+        Card(modifier = Modifier.padding(8.dp)) {
+            Text(text = "fifth card", modifier = Modifier.padding(8.dp))
+        }
 
     }
 
 
 }
-
-
-
-
-
-
-
 
 
 

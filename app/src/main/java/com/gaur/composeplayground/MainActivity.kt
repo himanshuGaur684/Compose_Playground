230f85b8cb3fae21dd99abcb14f43644cebb05fe
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2022-03-15 00:54:38 +0530

    Dropdown Menu in Jetpack Compose

--- app/src/main/java/com/gaur/composeplayground/MainActivity.kt
+++ app/src/main/java/com/gaur/composeplayground/MainActivity.kt
@@ -1,32 +1,25 @@
 package com.gaur.composeplayground
 
 import android.os.Bundle
-import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.gaur.composeplayground.ui.theme.ComposePlaygroundTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
 
@@ -34,93 +27,112 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePlaygroundTheme {
-                Surface(color = MaterialTheme.colors.background) {
-
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = "splash_screen") {
-                        composable("splash_screen") {
-                            SplashScreen(navController = navController)
-                        }
-
-                        composable("main_screen") {
-                            MainScreen()
-                        }
-
-                    }
-
-
-                }
+                MainScreen()
             }
         }
     }
-
-
-}
-
-
-@Composable
-fun SplashScreen(navController: NavController) {
-
-    val scale = remember { Animatable(0f) }
-
-    LaunchedEffect(key1 = true) {
-        scale.animateTo(
-            targetValue = 0.6f,
-            animationSpec = tween(
-                durationMillis = 700,
-                easing = {
-                    OvershootInterpolator(4f).getInterpolation(it)
-                }
-            )
-        )
-
-        delay(2000)
-        navController.navigate("main_screen") {
-            popUpTo("splash_screen") {
-                inclusive = true
-            }
-        }
-    }
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Blue)
-    ) {
-
-        Text(
-            text = "Vision Android Hindi",
-            style = TextStyle(Color.White, fontSize = 28.sp),
-            modifier = Modifier
-                .scale(scale.value)
-                .align(Alignment.Center)
-        )
-
-    }
-
-
 }
 
 
 @Composable
 fun MainScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-    ) {
 
-        Text(
-            text = "Main Screen",
-            style = TextStyle(Color.Black, fontSize = 28.sp),
-            modifier = Modifier
-                .align(Alignment.Center)
-        )
+    val list = listOf("one", "two", "three", "four", "five")
+    val expanded = remember { mutableStateOf(false) }
+    val currentValue = remember { mutableStateOf(list[0]) }
+
+
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+        Box(modifier = Modifier.fillMaxWidth()) {
+
+            Row(modifier = Modifier
+                .clickable {
+                    expanded.value = !expanded.value
+                }
+                .align(Alignment.Center)) {
+                Text(text = currentValue.value)
+                Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = null)
+
+
+                DropdownMenu(expanded = expanded.value, onDismissRequest = {
+                    expanded.value = false
+                }) {
+
+                    list.forEach {
+
+                        DropdownMenuItem(onClick = {
+                            currentValue.value = it
+                            expanded.value = false
+                        }) {
+
+                            Text(text = it)
+
+                        }
+
+                    }
+
+                }
+
+
+            }
+
+        }
+
 
     }
+
+
+
 }
 
 
+@Composable
+fun MainScreen2() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+        val list = listOf("one", "two", "three", "four", "five")
+        val currentValue = remember { mutableStateOf(list[0]) }
+        val expanded = remember { mutableStateOf(false) }
+
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Row(modifier = Modifier
+                .clickable {
+                    expanded.value = !expanded.value
+                }
+                .align(Alignment.Center)) {
+
+                Text(
+                    text = currentValue.value,
+                    style = TextStyle(color = Color.Black, fontSize = 20.sp)
+                )
+                Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = null)
 
 
+                DropdownMenu(expanded = expanded.value, onDismissRequest = {
+                    expanded.value = !expanded.value
+                }) {
+
+                    list.forEach {
+                        DropdownMenuItem(onClick = {
+                            expanded.value = false
+                            currentValue.value = it
+
+                        }) {
+                            Text(text = it)
+                        }
+                    }
+
+
+                }
+
+
+            }
+
+        }
+
+
+    }
+}
 
